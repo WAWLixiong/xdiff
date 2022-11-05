832e5baa337d47a0bc25d95faac550a5343160f2
zzlion <lixiong_zz@163.com>
2022-11-05 18:51:41 +0800

    design struct

---
+++ examples/config.rs
@@ -0,0 +1,8 @@
+use anyhow::Result;
+use xdiff::DiffConfig;
+
+fn main() -> Result<()> {
+    let content: &str = include_str!("../fixtures/test.yml");
+    let config = DiffConfig::from_yml(content)?;
+    Ok(())
+}
---
+++ src/config.rs
@@ -0,0 +1,50 @@
+use reqwest::{header::HeaderMap, Method};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use url::Url;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DiffConfig {
+    #[serde(flatten)]
+    pub profiles: HashMap<String, DiffProfile>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DiffProfile {
+    pub request1: RequestProfile,
+    pub request2: RequestProfile,
+    pub res: ResponseProfile,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct RequestProfile {
+    #[serde(with = "http_serde::method", default)]
+    pub method: Method,
+    pub url: Url,
+    #[serde(skip_serializing_if = "Option::is_none", default)]
+    pub params: Option<serde_json::Value>,
+    #[serde(
+        skip_serializing_if = "HeaderMap::is_empty",
+        with = "http_serde::header_map"
+    )]
+    pub headers: HeaderMap,
+    #[serde(skip_serializing_if = "Option::is_none", default)]
+    pub body: Option<serde_json::Value>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ResponseProfile {
+    #[serde(skip_serializing_if = "Vec::is_empty", default)]
+    pub skip_headers: Vec<String>,
+    pub skip_body: Vec<String>,
+}
+
+impl DiffConfig {
+    fn from_yml(yml_file: &str) -> anyhow::Result<Self> {
+        let profiles = HashMap::<String, DiffProfile>::new();
+        anyhow::Ok(DiffConfig {
+            profiles
+        })
+
+    }
+}
